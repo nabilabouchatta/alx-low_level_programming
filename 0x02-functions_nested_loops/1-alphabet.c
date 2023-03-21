1056a56e659fabf3581b2977cd5685a485df647a
hibara <nabilabouchatta111@gmail.com>
2023-03-21 18:46:16 +0000

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabets
+ * return 1
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = a; alpha <= z; alpha++)
+	{
+	_putchar(alpha);
+	}
+	_putchar('\n');
+}

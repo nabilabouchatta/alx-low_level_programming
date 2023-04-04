49780293606a0dd40bc11f011f5dbab4f245a0f1
bella <nabilabouchatta111@gmail.com>
2023-04-03 17:43:23 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strstr - locates a substring
+ * @haystack: string to serarch in
+ * @needle: substring to look for
+ *
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j])
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+			j++;
+		}
+		if (!needle[j])
+			return (&haystack[i]);
+		i++;
+	}
+	return (NULL);
+}

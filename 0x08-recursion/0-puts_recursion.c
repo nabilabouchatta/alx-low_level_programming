a5467862d381167e313c9011d15c651184ec6a9e
bella <nabilabouchatta111@gmail.com>
2023-04-05 16:07:07 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion -  prints a string
+ * @s: the string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		putchar(*s);
+		_puts_recursion(s + 1);
+
+	}
+	putchar('\n');
+}

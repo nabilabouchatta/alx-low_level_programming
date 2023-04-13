aaf7bff169684ccf8cb37f0cf2a0a158af86058a
bella <nabilabouchatta111@gmail.com>
2023-04-13 16:38:45 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory.
+ * @b: argument 
+ *
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *n:
+		n = malloc(b);
+	if (!p)
+		exit(98);
+	return (p);
+}

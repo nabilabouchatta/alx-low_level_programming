10c6ebdc69bb28fa61a145f8c30de4bace0c37c1
bella <nabilabouchatta111@gmail.com>
2023-04-19 17:11:58 -0700

    done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_endex - earches for an integer
+ * @array: array to search in
+ * @size: function pointer
+ *
+ * Return: the index of the first element for which the cmp function does not return 0
+ * If no element matches, return -1
+ * If size <= 0, return -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int n = 0;
+
+	if (array && cmp)
+	{
+		while (n < size)
+		{
+			if (cmp(array[n] != 0)
+					return (n);
+			n++;
+			}
+	}
+	return (-1);
+}

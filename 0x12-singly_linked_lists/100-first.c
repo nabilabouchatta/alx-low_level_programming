95ad4afbe3cd122fb024eaa981d3a6b89708a707
bella <nabilabouchatta111@gmail.com>
2023-04-28 16:29:23 -0700

    done

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
-#include "list.h"
+#include "lists.h"
 
 /**
  * callfirst - this function calls before main
+ *
  */
 void callfirst(void)
 {
 	printf(
-		"You're beat! and yet, you must allow,\n
-		I bore my house upon my back!\n"
-		);
+			"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+		  );
 }

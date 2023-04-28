6fded32e7b1d5a229c747e77d8f3d57b69bab3dc
bella <nabilabouchatta111@gmail.com>
2023-04-28 14:47:34 -0700

    done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,20 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t length = 0;
+	size_t how;
 
-	if (h == NULL)
-		return (0);
-	while (h->next)
+	how = 0;
+	while (h)
 	{
-		if (!h->str)
-			printf("[0] %s\n", "(nil)");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
+		how++;
 		h = h->next;
-		length++;
 	}
-	length++;
-	printf("[%d] %s\n", h->len, h->str);
-	return (length);
+	return (how);
 }

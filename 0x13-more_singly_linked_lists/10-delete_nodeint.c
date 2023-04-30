a08b0e9859eaa9311258459d2502fb4b99737feb
bella <nabilabouchatta111@gmail.com>
2023-04-29 17:39:12 -0700

    done

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -18,7 +18,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (index != 0)
 	{
-		for (i = 0; i < index - 1 && pre != NULL; i++)
+		for (i = 0; i < index - 1 && prev != NULL; i++)
 		{
 			prev = prev->next;
 		}

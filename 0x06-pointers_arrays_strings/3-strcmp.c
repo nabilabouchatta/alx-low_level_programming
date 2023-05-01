e51b88bd56a0f4d9455f698b3c3c57e801a73328
bella <nabilabouchatta111@gmail.com>
2023-04-30 17:53:20 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 
 	while (oper == 0)
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == `\0'))
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
 			break;
 		oper = *(s1 + i) - *(s2 + i);
 		i++;

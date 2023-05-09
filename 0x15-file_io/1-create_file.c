1fd638d43f6c3f0972edb9450d1beeb541bb661b
bella <nabilabouchatta111@gmail.com>
2023-05-09 15:50:42 -0700

    done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file -crates a file
+ * @filename: filename.
+ * @text_content: the content writed in the file.
+ * Return: 1 if it success. -1 if it fails.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int op, letters, wr;
+
+	if (!filename)
+		return (-1);
+
+	op = opent(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (op == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (letters = 0; text_content[letters]; letters++)
+		wr = write(op, text_content, letters);
+
+	if (rw == -1)
+		return (-1);
+
+	close(op);
+
+	return (1);
+}

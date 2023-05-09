1786af61c0950b797e3d400022764436c02cef6c
bella <nabilabouchatta111@gmail.com>
2023-05-09 15:54:24 -0700

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	op = opent(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (op == -1)
 		return (-1);

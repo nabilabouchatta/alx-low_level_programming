c336650ca33c29cd0a601a7d3fadeb2400c76246
bella <nabilabouchatta111@gmail.com>
2023-05-25 03:49:58 -0700

    done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,4 +15,5 @@ int _putchar(char);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
 #endif

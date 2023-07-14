33f41e5ef73f0fa345d3dd66c6352f1c9cc80b39
root <root@LAPTOP-0FFUURN6.localdomain>
2023-07-14 18:19:06 +0200

    2. Speak gently, she can hear mandatory

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
  *Return:1 on success and -1 on failure
  */
 
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, wr, n = 0;
 
